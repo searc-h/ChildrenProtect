55c60714cb61a79b8fb202a1e4938bbf65c5e29f
Ethereal-bang <939847757@qq.com>
2022-07-31 16:53:14 +0800

    update: <Set />

--- src/views/Set/Set.tsx
+++ src/views/Set/Set.tsx
@@ -1,5 +1,16 @@
 export default function Set (){
     return <>
-        Set Page
+        <form>
+            <label>
+                管理员账户名
+                <input placeholder={"15086861111"}/>    {/*原账户名*/}
+                <button type={"button"}>修改账户名</button>
+            </label>
+            <label>
+                管理员账户密码
+                <input type={"password"} placeholder={"*******"}/>    {/*原*/}
+                <button type={"button"}>修改密码</button>
+            </label>
+        </form>
     </>
 }
