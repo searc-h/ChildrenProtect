e414379d6989a389f02062a3ebdd55454d188aac
Ethereal-bang <939847757@qq.com>
2022-08-10 11:24:22 +0800

    update: <Data /> 地图数据

--- src/api/dataApi.ts
+++ src/api/dataApi.ts
@@ -1,5 +1,11 @@
 import {myAxios} from "./myAxios";
 
+// 数据概览
 export const showCardData = () => {
     return myAxios("admin/showDataCard");
+}
+
+// 地图数据
+export const showDataMap = () => {
+    return myAxios("admin/showDataMap");
 }
--- src/views/Data/Data.tsx
+++ src/views/Data/Data.tsx
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react'
 import './Data.css'
 import finished from '../../assets/icons/finished.png'
 import doing from '../../assets/icons/doing.png'
-import {showCardData} from "../../api/dataApi";
+import {showCardData, showDataMap} from "../../api/dataApi";
 import {message} from "antd";
 
 interface cardProps {
@@ -28,50 +28,12 @@ function MyCard(props: cardProps) {
 }
 
 interface map {
-  longitude : string | number   //经度
-  latitude :   string | number // 纬度
-  status  :  string  
+  longitude : number   //经度
+  latitude : number // 纬度
+  status  :  string
 }
 interface mapProps{
   mapList : map[]
-}
-function MyMap(props: mapProps) {
-  
-  let {mapList} = props
-  
-  useEffect(() => {
-    let map = new BMapGL.Map('mymap');
-    map.centerAndZoom(new BMapGL.Point(106.5907, 29.7320), 16);
-    map.enableScrollWheelZoom();
-    
-    let finishedIcon = new BMapGL.Icon(finished, new BMapGL.Size(45, 50));
-    let doingIcon = new BMapGL.Icon(doing, new BMapGL.Size(45, 54));
-
-    mapList.forEach((marker)=>{
-      let {longitude  , latitude} = marker
-      let Pointer :any
-      if(marker.status === 'finished')
-      {
-        // 创建点标记
-        Pointer = new BMapGL.Marker(new BMapGL.Point(+latitude ,+longitude ) ,{
-          icon: finishedIcon
-        })
-      }else{
-        // 创建点标记
-        Pointer = new BMapGL.Marker(new BMapGL.Point(+latitude ,+longitude ) ,{
-          icon: doingIcon
-        })
-      }
-
-      map.addOverlay(Pointer)
-    })
-
-  }, [mapList])
-
-  return (
-    <div id='mymap' style={{ "width": "100%", "height": "100%", "textAlign": "center", "backgroundColor": "#f3f3f3", "boxShadow": "0px 0px 10px rgba(0,0,0,0.2)" , zIndex:"99" }}>
-    </div>
-  )
 }
 
 export default function Data() {
@@ -80,8 +42,9 @@ export default function Data() {
     EventTotal: 0,
     PersonTotal: 0,
   });
+  const [mapList, setMapList] = useState<map[]>([]);
 
-  // cardList ， mapList 最后通过axios发到
+  // 卡片显示内容
   const cardList:cardProps[] = [
     {
       title: '强制报告事件总数',
@@ -100,29 +63,6 @@ export default function Data() {
     }
   ]
 
-  const mapList:map[] = [
-    {
-      longitude:  29.7332,  //经度
-      latitude:  106.5907, // 纬度
-      status: "finished" 
-    },
-    {
-      longitude:  29.7302,  //经度
-      latitude:  106.5930, // 纬度
-      status: "doing" 
-    },
-    {
-      longitude:  29.7322,  //经度
-      latitude:  106.5900, // 纬度
-      status: "doing" 
-    },
-    {
-      longitude:  29.7304,  //经度
-      latitude:  106.5947, // 纬度
-      status: "finished" 
-    }
-  ]
-
   // 获取数据概览
   useEffect(() => {
     showCardData().then(res => {
@@ -131,6 +71,101 @@ export default function Data() {
       return message.error(err.response.data.message);
     })
   }, [])
+
+  // 获取地图数据
+  useEffect(() => {
+    showDataMap().then(res => {
+      interface ResData {
+        Lon: number,
+        Lat: number,
+      }
+      interface Res {
+        Doing: Array<ResData>,
+        Finished: Array<ResData>,
+      }
+      const {Doing, Finished} = res.data.data as Res;
+      const obj: map[] = [];
+      Doing.forEach(item => {
+        obj.push({
+          latitude: item.Lat,
+          longitude: item.Lon,
+          status: "doing",
+        })
+      })
+      Finished.forEach(item => {
+        obj.push({
+          latitude: item.Lat,
+          longitude: item.Lon,
+          status: "finished",
+        })
+      })
+      obj.push(
+            {
+              longitude:  29.7302,  //经度
+              latitude:  150, // 纬度
+              status: "finished"
+            },
+      )
+      setMapList(obj);
+    }, err => {
+      return message.error(err.response.data.message);
+    })
+  }, [])
+
+  function MyMap(props: mapProps) {
+    let {mapList} = props
+    interface Coordinate {  // 坐标
+      ing: number,
+      lat: number,
+    }
+    const [center, setCenter] = useState<Coordinate>({
+      ing: 106.5907,
+      lat: 29.7320,
+    }); // 地图中心点
+
+    useEffect(() => { // 已返回任一点为中心点
+      if (mapList[0]) {
+        setCenter({
+          ing: mapList[0].longitude,
+          lat: mapList[0].latitude,
+        })
+      }
+    }, [mapList])
+
+    useEffect(() => {
+      let map = new BMapGL.Map('mymap');
+      map.centerAndZoom(new BMapGL.Point(center.ing, center.lat), 16);
+      map.enableScrollWheelZoom();
+
+      let finishedIcon = new BMapGL.Icon(finished, new BMapGL.Size(45, 50));
+      let doingIcon = new BMapGL.Icon(doing, new BMapGL.Size(45, 54));
+
+      mapList.forEach((marker)=>{
+        let {longitude  , latitude} = marker
+        let Pointer :any
+        if(marker.status === 'finished')
+        {
+          // 创建点标记
+          Pointer = new BMapGL.Marker(new BMapGL.Point(+latitude ,+longitude ) ,{
+            icon: finishedIcon
+          })
+        }else{
+          // 创建点标记
+          Pointer = new BMapGL.Marker(new BMapGL.Point(+latitude ,+longitude ) ,{
+            icon: doingIcon
+          })
+        }
+
+        map.addOverlay(Pointer)
+      })
+
+    }, [center.ing, center.lat, mapList])
+
+    return (
+        <div id='mymap' style={{ "width": "100%", "height": "100%", "textAlign": "center", "backgroundColor": "#f3f3f3", "boxShadow": "0px 0px 10px rgba(0,0,0,0.2)" , zIndex:"99" }}>
+        </div>
+    )
+  }
 
   return (
     <div className='data-outer'>
