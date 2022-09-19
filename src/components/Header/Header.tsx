ed2ed920182b62eb7a04549a1e12b9756aa25701
Ethereal-bang <939847757@qq.com>
2022-09-19 11:08:16 +0800

    update: UI - Header Component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,11 @@
-import avater from '../../assets/images/avatar.jpg'
-import msg from '../../assets/images/msg.png'
+import avatar from '../../assets/images/avatar.jpg'
 import {DownOutlined} from '@ant-design/icons'
 import { Dropdown, Menu, Space } from 'antd';
 import './Header.less'
 import { useNavigate } from 'react-router-dom';
 import removeToken from '../../utils/removeToken';
 export default function Header() {
-  
+
   const navigate = useNavigate()
   // 退出登陆
   const logout = ()=>{
@@ -27,28 +26,20 @@ export default function Header() {
     <header className='header'>
         <div className='left'>
           <div className='logo'>LOGO</div>
-          <div className='icon'>三</div>
           <div className='title'>“宝护未来”儿童权利保障智慧平台</div>
         </div>
 
         <div className='right'>
-            <div className="mes">
-              <img src={msg} alt="通知" width={35}/>
-            </div>
             <div className='avatar'>
-              <img src={avater} alt="头像"  width={40}/>
+              <img src={avatar} alt="头像"  width={40}/>
             </div>
-            <div className='nickname'>昵称</div>
-            <div className='logout'>
-
             <Dropdown overlay={menu} trigger={['click']}>
               <a onClick={e=>e.preventDefault()}>
                 <Space>
-                  <span className='other' style={{"color":"#fff"}}>其他</span><DownOutlined style={{'color':'#fff'}}/>
+                  <span className='other' style={{"color":"#fff"}}>管理员</span><DownOutlined style={{'color':'#fff'}}/>
                 </Space>
               </a>
             </Dropdown>
-            </div>
         </div>
     </header>
   )
